a93d73c18ea4522c07c8e2b57ed2b5d3bc459c61
jstao <7718454+talktao@user.noreply.gitee.com>
2022-04-01 17:54:43 +0800

    del：删除无用的log

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -2,9 +2,9 @@ const context = require.context("pages", true, /\.tsx$/);
 let components : any[] = [];
 context.keys().forEach((item: any, index: any) => {
     let path = item.split(".")[1].toLowerCase();
-    console.log(path,'path');
+
     let compareArr = path.split("/");
-    console.log(compareArr,'compareArr');  
+ 
     if (compareArr[1] === compareArr[2]) {
         path = "/" + compareArr[1];
     }
