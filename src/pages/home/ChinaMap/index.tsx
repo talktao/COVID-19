7aadac3333aef622e64108fb0fb143990bf3cbfc
jstao <7718454+talktao@user.noreply.gitee.com>
2022-04-01 17:53:58 +0800

    feat：地图配置更新

--- src/pages/home/ChinaMap/index.tsx
+++ src/pages/home/ChinaMap/index.tsx
@@ -20,86 +20,88 @@ const ChinaMap: React.FC = () => {
     const totalData = useContext(TotalContext) 
       
     useEffect(() => {
-      getCityList()  
+      	getCityList()  
     },[])
 
      // 获取各省数据
     const getCityList = async () => {
-      const result = await fetch('https://mock.yonyoucloud.com/mock/22022/COVID-19/getOnsInfo/list')
-      const data = await result.json()
-      const listData = data.data.areaTree[0].children.map((item:nowDataProps)=>({name:item.name, value: item.total.nowConfirm})) // 获取现有确诊人数数据
-      confirmRef.current = listData
-      initChart(isNow)     
+		const result = await fetch('https://mock.yonyoucloud.com/mock/22022/COVID-19/getOnsInfo/list')
+		const data = await result.json()
+		console.log(data.data,'datadata');
+		
+		const listData =  data.data.areaTree[0].children.map((item:nowDataProps)=>({name:item.name, value: item.total.nowConfirm})) // 获取现有确诊人数数据
+		confirmRef.current = listData
+		initChart(isNow)     
     } 
 
     const initChart = (target?:boolean) => {
 
-      const myChart = echarts.getInstanceByDom(chinaMapRef.current as unknown as HTMLDivElement);
-      chartInstance = myChart ? myChart : echarts.init(chinaMapRef.current as unknown as HTMLDivElement) //图表实例
-      let option:any = {
-        title: {
-          text: "全国疫情地图",
-          left: "center",
-          textStyle: {
-            color: "#9c0505"
-          }
-        },
-        tooltip: { // 提示框c
-          trigger: "item",
-          textStyle:{
-            align:'left'
-          },
-          formatter: (p:any) => {       
-            return p.data ? '省份：' + [p.data.name] + `${target ? '<br/>现存确诊：' :  '<br/>累计确诊：'}` + [p.data.value] 
-                                 :  [p.name]
-          }
-        },
-        series: [
-          {
-            type: 'map',
-            map: "china",
-            data: confirmRef.current,
-            label: {
-              show: true,
-              color: "black",
-              fontStyle: 10,
-              align: "left",
-            },
-            zoom: 1.2, // 当前缩放比例
-            roam: true, // 是否支持拖拽
-            itemStyle: {
-              borderColor: '#ccc', // 区域边框线
-              borderWidth: 1
-            },
-            emphasis: { // 高亮显示
-              label: {
-                color: "black",
-                fontSize: 10
-              },
-              itemStyle: {
-                areaColor: "lightyellow" // 区域高亮颜色
-              }
-            }
-          },
-        ],
-        visualMap: {
-          type: "piecewise",
-          show: true,
-          pieces: [
-            { min: 10000 },
-            { min: 1000, max: 9999 },
-            { min: 500, max: 999 },
-            { min: 100, max: 499 },
-            { min: 10, max: 99 },
-            { min: 1, max: 9 },
-            { value: 0 }
-          ],
-          inRange: {
-            color: ["#e2ebf4", "#ffe7b2", "#ffcea0", "#ffa577", "#ff6341", "#ff2736", "#de1f05"]
-          }
-        }
-      }
-      chartInstance.setOption(option);
+		const myChart = echarts.getInstanceByDom(chinaMapRef.current as unknown as HTMLDivElement);
+		chartInstance = myChart ? myChart : echarts.init(chinaMapRef.current as unknown as HTMLDivElement) //图表实例
+		let option:any = {
+			title: {
+				text: "全国疫情地图",
+				left: "center",
+				textStyle: {
+					color: "#9c0505"
+				}
+			},
+			tooltip: { // 提示框c
+			trigger: "item",
+			textStyle:{
+				align:'left'
+			},
+			formatter: (p:any) => {       
+				return p.data ? '省份：' + [p.data.name] + `${target ? '<br/>现存确诊：' :  '<br/>累计确诊：'}` + [p.data.value] 
+									:  [p.name]
+			}
+			},
+			series: [
+			{
+				type: 'map',
+				map: "china",
+				data: confirmRef.current,
+				label: {
+					show: true,
+					color: "black",
+					fontStyle: 10,
+					align: "left",
+				},
+				zoom: 1.2, // 当前缩放比例
+				roam: true, // 是否支持拖拽
+				itemStyle: {
+					borderColor: '#ccc', // 区域边框线
+					borderWidth: 1
+				},
+				emphasis: { // 高亮显示
+					label: {
+						color: "black",
+						fontSize: 10
+					},
+					itemStyle: {
+						areaColor: "lightyellow" // 区域高亮颜色
+					}
+				}
+			},
+			],
+			visualMap: {
+				type: "piecewise",
+				show: true,
+				pieces: [
+					{ min: 10000 },
+					{ min: 1000, max: 9999 },
+					{ min: 500, max: 999 },
+					{ min: 100, max: 499 },
+					{ min: 10, max: 99 },
+					{ min: 1, max: 9 },
+					{ value: 0 }
+				],
+				inRange: {
+					color: ["#e2ebf4", "#ffe7b2", "#ffcea0", "#ffa577", "#ff6341", "#ff2736", "#de1f05"]
+				}
+			}
+		}
+		chartInstance.setOption(option);
     }
 
     // 切换地图（现存确诊，累计确诊）
